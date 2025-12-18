01f3d1da3765d53a11d98c6c932bc46a3d2e1d1d
qian <ruiqian.tang@seeed.cc>
2025-12-18 17:33:23 +0800

    feat: Add a link to the ChirpStack application server.

--- feeds/lorawan-gateway/luci-app-lora/htdocs/luci-static/resources/view/lora/lora-platform/chirpstack.js
+++ feeds/lorawan-gateway/luci-app-lora/htdocs/luci-static/resources/view/lora/lora-platform/chirpstack.js
@@ -33,6 +33,13 @@ return view.extend({
             return 'Please enter a hostname:port';
         }
 
+        var link = s.option(form.DummyValue, '_link', _(' '));
+        link.rawhtml = true;
+        link.cfgvalue = function(section_id) {
+            var baseURL = window.location.hostname;
+            return '<a href="http://' + baseURL + ':8080" target="_blank">' + _('Open ChirpStack Application Server') + '</a>';
+        };
+
         return mMap;
     }
 });
